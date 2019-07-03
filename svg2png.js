eb5d6c00f77e76fbc9d79fbe52bc56c79925ab45
liao961120 <liao961120@gmail.com>
2019-07-03 23:46:16 +0800

    deal with PNG resolution: add scale factor opt

--- svg2png.js
+++ svg2png.js
@@ -1,10 +1,15 @@
+var SVGs = [];
+
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.download').forEach(item => item.style.display = 'none');
+    document.querySelectorAll('.convert').forEach(item => item.style.display = 'none');
 
+    // Preload image
     // Get uploaded files
     document.querySelector('input#uploads').onchange = function() {
         var files = this.files;
-        for (i=0; i < files.length; ++i) {
+        for (let i=0; i < files.length; ++i) {
             
             // Write download link
             var filename = files[i].name.split('.').slice(0, -1).join('.')
@@ -15,29 +20,62 @@ document.addEventListener('DOMContentLoaded', () => {
             link.target = '_blank';
             link.download = `${filename}.png`;
             link.innerHTML = `${filename}.png`;
+            link.id = filename.replace(/ +/g, '-');
             link.className = 'downloadLink';
-
-            // Render svg to canvas
-            canvg(document.querySelector('#canvas'), URL.createObjectURL(files[i]));
-            // Save as png
-            canvas = document.querySelector('#canvas');
-            var img = canvas.toDataURL("image/png");
-            link.href = img;
-            
             // Release objectURL for next loop
             link.onload = function() {
                 URL.revokeObjectURL(this.src);
             }
+
+            // load img to get aspect ratio
+            var img = document.createElement('img');
+            img.src = URL.createObjectURL(files[i]);
+            img.linkID = link.id;
+            img.style.display = 'none';
+            img.onload = function() {
+                SVGs.push({linkID: this.linkID, width: this.width, height: this.height, dataURL: this.src})
+            };
+            document.querySelector('body').append(img)
         };
-        
-        // batch download
-        document.querySelector('#download').onclick = () => {
-            document.querySelectorAll('.downloadLink').forEach(
-                item => item.click()
-            );
-        };
-    
-    // Show download button
-    document.querySelectorAll('.download').forEach(item => item.style.display = 'inline');
+
+    // Show convert button
+    document.querySelectorAll('.convert').forEach(item => item.style.display = 'inline');
+    };
+
+    // Convert image to svg
+    document.querySelector('#convert').onclick = () => {
+        // Read Scale factor
+        var SF = parseFloat(document.querySelector('input#SF').value);  
+        if (!document.querySelector('input#SF').value)
+            SF = 1;
+
+        // Render svgs
+        for (let j=0; j < SVGs.length; ++j) {
+            // Render svg to canvas
+            // Get chart aspect ratio
+
+            // Set canvas size
+            canvas = document.querySelector('#canvas');
+            canvas.width = SVGs[j].width * SF;
+            canvas.height = SVGs[j].height * SF;
+
+            canvg(canvas, SVGs[j].dataURL, {
+                ignoreDimensions: true,
+                scaleWidth: canvas.width,
+                scaleHeight: canvas.height
+            });
+
+            // Save as png
+            var img = canvas.toDataURL("image/png");
+            document.querySelector(`#${SVGs[j].linkID}`).href = img;
+
+            // Show download button
+            document.querySelectorAll('.download').forEach(item => item.style.display = 'inline');
+        }
+    };
+
+    // batch download
+    document.querySelector('#download').onclick = () => {
+        document.querySelectorAll('.downloadLink').forEach(item => item.click());
     };
 });
